4a9d79e052df4ab616a72ed033bf7b18abe33132
ladymae.manuncia <ladymae.manuncia@tfisolutions.com>
2022-03-18 13:59:27 +0800

    Second Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My portfolio
+          My portfolio...
         </p>
         <a
           className="App-link"
